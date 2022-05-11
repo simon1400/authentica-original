19f2e5105a7b3448b2350a2b60a6c8a2d66800db
Your Name <>
2022-05-11 19:22:02 +0200

    sdfdsf

--- styles/BigImage.ts
+++ styles/BigImage.ts
@@ -3,16 +3,18 @@ import styled from "@emotion/styled";
 const BigImage = styled.div`
   position: relative;
   overflow: hidden;
-  height: 695px;
+  /* height: 695px; */
+  width: 100%;
+  max-width: 100%;
   img{
-    position: absolute;
+    /* position: absolute;
     object-fit: cover;
-    object-position: center;
+    object-position: center; */
     width: 100%;
-    height: 100%;
+    /* height: 100%;
     left: 50%;
     top: 50%;
-    transform: translate(-50%, -50%);
+    transform: translate(-50%, -50%); */
   }
 `
 
